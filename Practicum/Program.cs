0d615514f680ee46df9557d3706c6795845d06d0
Ярослав Богачев <bogachev762@outlook.com>
2023-04-19 16:41:55 +0500

    Добавьте файлы проекта.

---
+++ Practicum/Program.cs
@@ -0,0 +1,102 @@
+using System.IO;
+using System.Reflection.PortableExecutable;
+using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
+
+// начальные данные
+
+internal class Program
+{
+    public static List<Person> users = new List<Person>();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+        app.UseInput();
+        app.Run(async (context) =>
+        {
+            var response = context.Response;
+            var request = context.Request;
+            var path = request.Path;
+            if (path == "/user" && request.Method == "POST")
+            {
+                response.ContentType = "text/html; charset=utf-8";
+                IFormFileCollection files = request.Form.Files;
+                // путь к папке, где будут храниться файлы
+                var uploadPath = $"{Directory.GetCurrentDirectory()}/uploads";
+                // создаем папку для хранения файлов
+                Directory.CreateDirectory(uploadPath);
+
+                foreach (var file in files)
+                {
+                    // путь к папке uploads
+                    string fullPath = $"{uploadPath}/{file.FileName}";
+
+                    // сохраняем файл в папку uploads
+                    using (var fileStream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                }
+                Console.WriteLine("ggjmvjg");
+                await response.WriteAsync("<p>Файлы успешно загружены fvfvfbgbghfv</p>");
+            }
+            else
+            {
+                response.ContentType = "text/html; charset=utf-8";
+                await response.SendFileAsync("html/files.html");
+            }
+        });
+        app.Run();
+    }
+}
+
+public class InputMiddleware
+{
+    private readonly RequestDelegate next;
+
+    public InputMiddleware(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var response = context.Response;
+        var request = context.Request;
+        var path = request.Path;
+        response.ContentType = "text/html; charset=utf-8";
+
+        if (path == "/user")
+        {
+            var b = request.Query;
+            if (!Program.users.Contains(new Person { Name = b["login"], Password = b["password"] }))
+                Program.users.Add(new Person { Name = b["login"], Password = b["password"] });
+            await response.WriteAsync($"{request.Query["login"]} - {request.Query["password"]}");
+            response.ContentType = "text/html; charset=utf-8";
+            await next.Invoke(context);
+        }
+        else
+        {
+            response.ContentType = "text/html; charset=utf-8";
+            await response.SendFileAsync("html/index.html");
+        }
+    }
+}
+
+public static class InputExtensions
+{
+    public static IApplicationBuilder UseInput(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<InputMiddleware>();
+    }
+}
+
+public class Person
+{
+    
+    public string Name { get; set; } = "";
+    public string Password { get; set; } = "";
+}
+
+
