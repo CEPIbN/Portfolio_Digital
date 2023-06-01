9ec0a8cbd8810614f8d50f3530431fa675dc9f0e
Ярослав Богачев <bogachev762@outlook.com>
2023-06-01 12:14:19 +0500

    api controller1

--- Practicum/Controllers/UserApiController.cs
+++ Practicum/Controllers/UserApiController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVP.Models;
 
 namespace MVP.Controllers
 {
@@ -7,5 +10,24 @@ namespace MVP.Controllers
     [ApiController]
     public class UserApiController : ControllerBase
     {
+        private ApplicationContext db;
+        public UserApiController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public async Task GetData()
+        {
+            var userName = User.Identity.Name;
+            var user = db.Users.FirstOrDefaultAsync(item => item.Email == userName);
+            if (user == null) 
+            {
+                Response.StatusCode = 404;
+                await Response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+            }
+            else
+            {
+                await Response.WriteAsJsonAsync(user);
+            }
+        }
     }
 }
