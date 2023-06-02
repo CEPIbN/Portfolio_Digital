d108d1d80f797c327af83bde648455f4f1e22422
mimmooowo <abozin.kirill@mail.ru>
2023-06-03 02:06:23 +0500

    Добавлено представление "Information". Скрипт

---
+++ Practicum/wwwroot/js/script.js
@@ -0,0 +1,58 @@
+﻿
+async function getData() {
+    try {
+        const response = await fetch('/api/UserApi/GetData');
+        if (response.ok) {
+            const user = await response.json();
+            document.getElementById('name').value = user.name;
+            document.getElementById('lastName').value = user.lastName;
+            document.getElementById('phoneNumber').value = user.phoneNumber;
+            document.getElementById('age').value = user.age;
+        } else {
+            const error = await response.json();
+            console.error('Ошибка получения данных:', error.message);
+        }
+    } catch (error) {
+        console.error('Ошибка получения данных:', error);
+    }
+}
+
+
+async function updateData() {
+    const name = document.getElementById('name').value;
+    const lastName = document.getElementById('lastName').value;
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    const age = document.getElementById('age').value;
+
+    const userData = {
+        name,
+        lastName,
+        phoneNumber,
+        age
+    };
+
+    try {
+        const response = await fetch('/api/UserApi/Update', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+
+        if (response.ok) {
+            const user = await response.json();
+            document.getElementById('name').value = user.name;
+            document.getElementById('lastName').value = user.lastName;
+            document.getElementById('phoneNumber').value = user.phoneNumber;
+            document.getElementById('age').value = user.age;
+        } else {
+            const error = await response.json();
+            console.error('Ошибка обновления данных:', error.message);
+        }
+    } catch (error) {
+        console.error('Ошибка обновления данных:', error);
+    }
+}
+
+getData();
