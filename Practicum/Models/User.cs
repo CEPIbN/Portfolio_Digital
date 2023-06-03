43b8e8198b4cf72ed4d73be50ea571fd0347eb76
Ярослав Богачев <bogachev762@outlook.com>
2023-06-03 17:49:33 +0500

    апдейт4

--- Practicum/Models/User.cs
+++ Practicum/Models/User.cs
@@ -4,19 +4,28 @@ namespace MVP.Models
 {
     public class User
     {
-        public byte[]? Avatar { get; set; }
-        public string? Name { get; set; }
-        public string? LastName { get; set; }
-        public string? PhoneNumber { get; set; }
+        public byte[] Avatar { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
         public int Age { get; set; }
         public int Id { get; set; }
         [Required]
-        public string? Login { get; set; }
+        public string Login { get; set; }
         [Required]
-        public string? Password { get; set; }
+        public string Password { get; set; }
         [Required]
         [EmailAddress]
-        public string? Email { get; set; }
-
+        public string Email { get; set; }
+        public User()
+        {
+            Avatar = new byte[0];
+            Name = "";
+            LastName = "";
+            PhoneNumber = "";
+            Login = "";
+            Password = "";
+            Email = "";
+        }
     }
 }
