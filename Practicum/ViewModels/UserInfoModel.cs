820ec144903a8b22e3ab293b16f8a0d3343c7fbe
Ярослав Богачев <bogachev762@outlook.com>
2023-06-03 19:10:59 +0500

    update

--- Practicum/ViewModels/UserInfoModel.cs
+++ Practicum/ViewModels/UserInfoModel.cs
@@ -2,10 +2,10 @@
 {
     public class UserInfoModel
     {
-        public IFormFile? Avatar { get; set; }
+        //public IFormFile? Avatar { get; set; }
         public string? Name { get; set; }
         public string? LastName { get; set; }
         public string? PhoneNumber { get; set; }
         public int Age { get; set; }
-    }
+    } 
 }
