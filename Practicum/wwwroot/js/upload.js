39ea570e79304670807e36bc4dacb1e33a547e10
Ярослав Богачев <bogachev762@outlook.com>
2023-06-11 18:20:00 +0500

    upload

--- Practicum/wwwroot/js/upload.js
+++ Practicum/wwwroot/js/upload.js
@@ -1,24 +1,23 @@
-<script>
-    document.getElementById('uploadForm').addEventListener('submit', function(e) {
-        e.preventDefault();
+document.getElementById('uploadForm').addEventListener('submit', function (e) {
+    e.preventDefault();
 
-    var form = document.getElementById('uploadForm');
+    var form = document.getElementById("uploadForm");
     var formData = new FormData(form);
 
     fetch('api/UserApi/UploadFile', {
-        method: 'POST',
-    body: formData
-            })
-    .then(function(response) {
-                if (response.ok) {
-        alert('Файл успешно загружен');
-    form.reset();
-                } else {
-        alert('Не удалось загрузить файл!');
-                }
-            })
-    .catch(function(error) {
-        console.error('Ошибка:', error);
-            });
+        method: "POST",
+        body: formData
+    })
+        .then(function (response) {
+            if (response.ok) {
+                alert('Файл успешно загружен');
+                form.reset();
+            }
+            else {
+                alert('Не удалось загрузить файл!');
+            }
+        })
+        .catch(function (error) {
+            console.error('Ошибка:', error);
         });
-</script>
+});
