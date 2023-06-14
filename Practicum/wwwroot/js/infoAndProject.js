e4a418ef39286dac5fc4337348b39ccb22f6d126
mimmooowo <abozin.kirill@mail.ru>
2023-06-14 13:48:21 +0500

    Добавление проектов

---
+++ Practicum/wwwroot/js/infoAndProject.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function() {
+    $.get('/api/UserApi/GetData', function(data) {
+        var user = data.User;
+        $('#name').text(user.Name);
+        $('#last-name').text(user.LastName);
+        $('#phone-number').text(user.PhoneNumber);
+        $('#age').text(user.Age);
+        $('#email').text(user.Email);
+
+        var projects = data.FileData;
+        var projectsList = $('#projects-list');
+
+        for (var i = 0; i < projects.length; i++) {
+            var project = projects[i];
+            var fileName = project.fileName;
+            var description = project.Description;
+
+            var listItem = $('<li>');
+            var fileNameElement = $('<h3>').text(fileName);
+            var descriptionElement = $('<p>').text(description);
+
+            listItem.append(fileNameElement, descriptionElement);
+            projectsList.append(listItem);
+        }
+    });
+});
